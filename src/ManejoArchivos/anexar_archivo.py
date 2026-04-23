e9af760040ec1f71b6db943d088e71b0ed22ea9e
Andres Lopez <144001874+SAndresLopez@users.noreply.github.com>
2026-04-23 00:05:04 -0500

    Delete src directory

--- src/ManejoArchivos/anexar_archivo.py
+++
@@ -1,8 +0,0 @@
-print("*** Anexar información")
-
-nombre_archivo = "mi_archivo.txt"
-with open(nombre_archivo, "a") as archivo:
-    archivo.write("\nAnexando informacion...")
-    archivo.write("\nSaliendo de anexar informacion...")
-
-print(f"Se ha anexado información al archivo {nombre_archivo}")
--- src/Tkinter/MostrarVentana.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-ventana = tk.Tk()
-ventana.geometry("600x400")
-ventana.title("Nueva ventana")
-ventana.resizable(0,0)
-ventana.configure(background="#1d2d44")
-ventana.mainloop()
